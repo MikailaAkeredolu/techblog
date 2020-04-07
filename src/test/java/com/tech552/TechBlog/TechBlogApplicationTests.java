37e6010cb30197851624c382b74cbb53cb938315
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-04-07 15:56:39 -0400

    initial commit for springboot blog

---
+++ src/test/java/com/tech552/TechBlog/TechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech552.TechBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
