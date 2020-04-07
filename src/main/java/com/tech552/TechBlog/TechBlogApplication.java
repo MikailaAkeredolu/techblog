2e3f6ae67994a3a0ede345036aa7ed500fcc86ba
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-04-07 16:09:22 -0400

    welcome message feature done

--- src/main/java/com/tech552/TechBlog/TechBlogApplication.java
+++ src/main/java/com/tech552/TechBlog/TechBlogApplication.java
@@ -8,6 +8,7 @@ public class TechBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechBlogApplication.class, args);
+		System.out.println("welcome message available in welcome branch");
 	}
 
 }
